edc8d1b678d2116dba7810e407b6bd9ae4ba71d9
Fernando Tomasio <fernandotomasio@gmail.com>
2020-04-27 21:02:57 -0300

    add category page

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using t_money.Models;
+
+namespace t_money.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ILogger<CategoryController> _logger;
+
+        public CategoryController(ILogger<CategoryController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
