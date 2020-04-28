0809ed7b417f9d373c17953221feefbc9d65f2a7
Fernando Tomasio <fernandotomasio@gmail.com>
2020-04-27 21:03:10 -0300

    add budget page

---
+++ Controllers/BudgetController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using t_money.Models;
+
+namespace t_money.Controllers
+{
+    public class BudgetController : Controller
+    {
+        private readonly ILogger<BudgetController> _logger;
+
+        public BudgetController(ILogger<BudgetController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
