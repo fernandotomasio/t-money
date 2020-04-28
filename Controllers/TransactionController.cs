aaf9e5b0fd12c2b98ba413c1166bcbef86b5b19d
Fernando Tomasio <fernandotomasio@gmail.com>
2020-04-27 21:01:33 -0300

    add transaction page

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using t_money.Models;
+
+namespace t_money.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly ILogger<TransactionController> _logger;
+
+        public TransactionController(ILogger<TransactionController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
