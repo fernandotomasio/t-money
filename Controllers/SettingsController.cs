ca5e3be729891acff0f95d0765fe1b8106c739a2
Fernando Tomasio <fernandotomasio@gmail.com>
2020-04-30 08:10:40 -0300

    add settings page

---
+++ Controllers/SettingsController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using t_money.Models;
+
+namespace t_money.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private readonly ILogger<SettingsController> _logger;
+
+        public SettingsController(ILogger<SettingsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+    }
+}
