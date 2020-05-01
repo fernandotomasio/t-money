cdc3c284c57fbc7a128b402001ed24ff2d72e92f
Fernando Tomasio <fernandotomasio@gmail.com>
2020-05-01 18:24:02 -0300

    Add model classes

---
+++ Models/BudgetItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace t_money.Models
+{
+    public class BudgetItem
+    {
+        public Budget Budget { get; set; }
+        public Category Category { get; set; }
+        public double Amount { get; set; }
+
+
+    }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace t_money.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public Account Account { get; set; }
+        public Category Category { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+
+    }
+}
