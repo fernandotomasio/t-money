5b29559fd71dab806d5ba6ed1cfedb83b5fbd5d9
Fernando Tomasio <fernandotomasio@gmail.com>
2020-05-03 12:58:49 -0300

    add identity scaffold

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using t_money.Areas.Identity.Data;
+using TMoney.Data;
+
+[assembly: HostingStartup(typeof(t_money.Areas.Identity.IdentityHostingStartup))]
+namespace t_money.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AppIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AppIdentityContextConnection")));
+
+                services.AddDefaultIdentity<AppIdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AppIdentityContext>();
+            });
+        }
+    }
+}
